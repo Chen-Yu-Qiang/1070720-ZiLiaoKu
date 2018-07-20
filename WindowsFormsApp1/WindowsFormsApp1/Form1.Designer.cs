a5f9c42d3afe0e0f460082ae770bedb19028de18
YuQiang <me,yuqiangchen@gmail.com>
2018-07-20 15:39:32 +0800

    拉大

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -115,7 +115,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(13, 239);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(512, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
